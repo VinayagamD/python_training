97acd6330b358a931d7f2c1ecd54615127d11b86
duraiv2 <vinayagam.durairaj@medtronic.com>
2018-10-12 09:18:23 +0530

    oops Ans Strings

--- strings/MyString.py
+++ strings/MyString.py
@@ -6,5 +6,5 @@ for i in range(0, len(str)):
     print(str[i], end="\t")
 print()
 length = len(str)
-for i in range(-1,(-length-1), -1):
+for i in range(-1, (-length-1), -1):
     print(str[i], end="\t")
