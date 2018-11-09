e020183e1b13f716c727d294f46d1016729b2bf8
duraiv2 <vinayagam.durairaj@medtronic.com>
2018-11-09 16:16:29 +0530

    tuple other operation

--- commons/utils.py
+++ commons/utils.py
@@ -4,4 +4,13 @@ class Utils:
     def print_message(message, line="start"):
         print(f"==================== {message} {line} ===========================\n")
 
+    """
+        @staticmethod
+        def print_message(message, end_line=False):
+            print(f"==================== {message} ", "start" if end_line else "start",
+             " ===========================\n")
+    """
 
+    @staticmethod
+    def print_divider():
+        print("====================================================================================================\n")
--- tuples/mytuple.py
+++ tuples/mytuple.py
@@ -35,3 +35,69 @@ print(tupl1)
 print(tupl2)
 my_tuple_message("Tuples with tuple", "end")
 
+
+# Accessing Tuples
+my_tuple_message("Accessing Tuples")
+data1 = (1, 2, 3, 4)
+data2 = ('x', 'y', 'z')
+print(data1[0])
+print(data1[0:2])
+print(data2[-3:-1])
+print(data1[0:])
+print(data2[:2])
+my_tuple_message("Accessing Tuples", "end")
+
+Utils.print_divider()
+
+# Python Tuple Operation
+my_tuple_message(" Tuple Operation")
+
+Utils.print_divider()
+# Adding Tuples
+my_tuple_message(" Adding Tuple")
+data1 = (1, 2, 3, 4)
+data2 = ('x', 'y', 'z')
+data3 = data1 + data2
+print(data1)
+print(data2)
+print(data3)
+my_tuple_message(" Adding Tuple", "end")
+
+# Replicating Tuple
+my_tuple_message(" Replicating Tuple")
+tuple1 = (10, 20, 30)
+tuple2 = (40, 50, 60)
+print(tuple1*2)
+print(tuple2*3)
+my_tuple_message(" Replicating Tuple", "end")
+
+my_tuple_message(" Tuple Operation", "end")
+
+# Python tuple Slicing
+my_tuple_message("tuple slicing")
+data1 = (1, 2, 4, 5, 7)
+print(data1[0:2])
+print(data1[4])
+print(data1[:-1])
+print(data1[-5:])
+print(data1)
+my_tuple_message("tuple slicing", "end")
+
+# Python Tuple Other Operation
+my_tuple_message(" Tuple Other Operation")
+
+# Creating tuple From Existing
+
+data1 = (10, 20, 30)
+data2 = (40, 50, 60)
+data3 = data1+data2
+print(data3)
+my_tuple_message(" Tuple Other Operation", "end")
+
+# deleting tuple
+my_tuple_message(" deleting ")
+data = (10, 20, 'rahul', 40.6, 'z')
+print(data)
+del data
+# print(data)
+my_tuple_message(" deleting ", "end")
