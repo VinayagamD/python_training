01e2b817d1d6b8faf3f6da94948395ce22e15b5d
duraiv2 <vinayagam.durairaj@medtronic.com>
2018-11-13 16:42:35 +0530

    Function Beginning

--- functions/my_functions.py
+++ functions/my_functions.py
@@ -11,7 +11,7 @@ function_message("definition")
 
 def my_sum(x, y):
     """ Going to add x and y"""
-    s = x+y
+    s = x + y
     print("Sum of two numbers is")
     print(s)
 
@@ -22,7 +22,6 @@ my_sum(20, 30)
 my_sum(10, 20)
 function_message("calling function", "end")
 
-
 function_message("definition", "end")
 
 Utils.print_divider()
@@ -30,4 +29,26 @@ Utils.print_divider()
 # Return
 function_message("return")
 
+
+def sum(a, b):
+    """Adding the two values"""
+    print("Printing within function")
+    print(a + b)
+    return a + b
+
+
+def msg():
+    print("Hello")
+    return
+
+
+total = sum(10, 20)
+
+print("Printing Outside", total)
+msg()
 function_message("return", "end")
+
+Utils.print_divider()
+
+
+
