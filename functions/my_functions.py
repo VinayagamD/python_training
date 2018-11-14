ff1f9afca23afe3d56aabbf6d08ed1f7e16819aa
duraiv2 <vinayagam.durairaj@medtronic.com>
2018-11-14 09:27:27 +0530

    Function complete

--- functions/my_functions.py
+++ functions/my_functions.py
@@ -30,7 +30,7 @@ Utils.print_divider()
 function_message("return")
 
 
-def sum(a, b):
+def my_sum(a, b):
     """Adding the two values"""
     print("Printing within function")
     print(a + b)
@@ -42,7 +42,7 @@ def msg():
     return
 
 
-total = sum(10, 20)
+total = my_sum(10, 20)
 
 print("Printing Outside", total)
 msg()
@@ -50,5 +50,105 @@ function_message("return", "end")
 
 Utils.print_divider()
 
+# Function Argument and parameter
 
+function_message("Function Argument and parameter")
+
+
+def addition(x, y):
+    print(x+y)
+
+
+x = 15
+addition(x, 10)
+addition(x, x)
+y = 20
+addition(x, y)
+function_message("Function Argument and parameter", "end")
+
+
+# Passing Parameter
+
+function_message("Passing Parameter")
+
+
+def my_sum(a, b):
+    """Function having two parameters"""
+    c = a+b
+    print(c)
+
+
+my_sum(10, 20)
+# my_sum(20)
+
+function_message("Passing Parameter", "end")
+
+
+# Default arguments
+function_message("Default arguments")
+
+
+def msg(id, name, age=21):
+    """Printing the passed value"""
+    print(id)
+    print(name)
+    print(age)
+    return
+
+
+msg(id=100, name='Ravi', age= 20)
+msg(id=101, name='Ratan')
+
+function_message("Default arguments", "end")
+
+Utils.print_divider()
+
+# Anonymous Function
+
+function_message("Anonymous Function")
+
+square = lambda x1: x1*x1
+
+print("Square number is ", square(10))
+
+function_message("Anonymous Function", "end")
+
+# Scope of variable
+function_message(" Scope of variables")
+
+# Local Scope
+
+function_message("Local Scope")
+
+
+def msg():
+    a = 10
+    print("Value of a is ", a)
+    return
+
+
+msg()
+
+# print(a) # Error line
+
+function_message("Local Scope", "end")
+
+# Global Variable
+function_message("Global Scope")
+
+b = 20
+
+
+def msg():
+    a = 10
+    print("Value of a is ", a)
+    print("Value of b is ", b)
+    return
+
+
+msg()
+print(b)
+
+function_message("Global Scope", "end")
+function_message(" Scope of variables", "end")
 
