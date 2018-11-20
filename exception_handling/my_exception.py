ffe98579e0e4fc05c8cbb5dc916e59c08cdca369
duraiv2 <vinayagam.durairaj@medtronic.com>
2018-11-20 10:33:10 +0530

    Exception Complete

--- exception_handling/my_exception.py
+++ exception_handling/my_exception.py
@@ -59,4 +59,19 @@ exception_message(" Raise An Exception", "end")
 # Custom Exception
 exception_message("Custom Exception")
 
+
+class ErrorInCode(Exception):
+
+    def __init__(self, data):
+        self.data = data
+
+    def __str__(self):
+        return repr(self.data)
+
+
+try:
+    raise ErrorInCode(2000)
+except ErrorInCode as ae:
+    print("Received Error : ",ae.data)
+
 exception_message("Custom Exception", "end")
