00feb8881df0531bf1480900bc40ca8d1211fd42
duraiv2 <vinayagam.durairaj@medtronic.com>
2018-11-22 18:45:23 +0530

    Dates in progress

--- dates/my_dates.py
+++ dates/my_dates.py
@@ -7,13 +7,39 @@ def dates_message(message, line='start'):
 
 
 # localtime
-Utils.print_message("localtime")
+dates_message("localtime")
 localtime = time.localtime(time.time())
 print("Current time is: ", localtime)
-Utils.print_message("localtime", "end")
+dates_message("localtime", "end")
 
 # formatted time
-Utils.print_message("formatted time")
+dates_message("formatted time")
 localtime = time.asctime(time.localtime(time.time()))
 print("Formatted Time", localtime)
-Utils.print_message("formatted time", "end")
+dates_message("formatted time", "end")
+
+# Print Time
+dates_message("Print Current Time")
+print("Current Time ", time.time())
+dates_message("Print Current Time", "end")
+
+
+# Print ASCII Time
+dates_message("Ascii Time")
+t = time.localtime()
+print(time.asctime(t))
+dates_message("Ascii Time", "end")
+
+# sleep
+dates_message("sleep")
+print(time.asctime(time.localtime(time.time())))
+time.sleep(10)
+print(time.asctime(time.localtime(time.time())))
+dates_message("sleep", "end")
+
+# strptime
+dates_message("strptime")
+timerequired = time.strptime("26 Jun 14", "%d %b %y")
+print(timerequired)
+dates_message("strptime", "end")
+
